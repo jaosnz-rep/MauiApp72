8922a53aba35dee50340de4d97483c1d9da89e73
jaosnz-rep <82628041+jaosnz-rep@users.noreply.github.com>
2024-05-11 17:24:26 +0800

    Add files via upload

---
+++ MauiApp72/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp72
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiApp72/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp72
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
